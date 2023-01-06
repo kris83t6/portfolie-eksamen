01553c65552e79854b4de6d5a26fed0e9bc0ebcd
Kristine Laursen <kris83t6@stud.kea.dk>
2023-01-06 17:23:26 +0100

    Opsætning af sider

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 /*** Hamburger ***/
- const hamburger = document.querySelector(".hamburger");
- const navMenu = document.querySelector(".nav-menu ");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu ");
 
- hamburger.addEventListener("click", () => {
-   hamburger.classList.toggle("active");
-   navMenu.classList.toggle("active");
- });
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
 
- /*** Lukker hamburger når der klikkes på links ***/
- document.querySelectorAll(".nav-link").forEach((n) =>
-   n.addEventListener("click", () => {
-     hamburger.classList.remove("active");
-     navMenu.classList.remove("active");
-   })
- );
+/*** Lukker hamburger når der klikkes på links ***/
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
+//Forside billede
+
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+  console.log("sidenVises");
+
+  document.querySelector("#forside_container").classList.add("drej");
+}
+
+document.querySelector("#forside_container").addEventListener("animationiteration", drejEnIteration);
